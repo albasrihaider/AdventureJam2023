d214b2600290678bbbcef981367b583661b6dde2
Haider Albasri <haiderf3@gmail.com>
2023-05-30 23:06:20 +0300

    Updated Room and RoomController Classes

--- AdventureGJ2023/Assets/Scripts/DungeonGeneration/Room.cs
+++ AdventureGJ2023/Assets/Scripts/DungeonGeneration/Room.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Room : MonoBehaviour
 {
+
+    public int Height, Width, X, Y;
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        if (RoomController.instance == null)
+        {
+            Debug.Log("Pressed play in the wrong scene!");
+            return;
 
-    // Update is called once per frame
-    void Update()
+        }
+    }
+    private void OnDrawGizmos()
     {
-        
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector3(Width, Height, 0));
+    }
+    public Vector3 GetRoomCentre()
+    {
+        return new Vector3(X * Width, Y * Height, 0);
     }
 }
--- AdventureGJ2023/Assets/Scripts/DungeonGeneration/RoomController.cs
+++ AdventureGJ2023/Assets/Scripts/DungeonGeneration/RoomController.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement;  
+public class RoomInfo 
+{
+        public string name;
+        public int X, Y;
+}
+   
 public class RoomController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public static RoomController instance;
+
+    string currentWorldName = "Basement";
+
+    RoomInfo currentLoadRoomData;
+
+    Queue<RoomInfo> loadRoomQueue = new Queue<RoomInfo>();
+
+    public List<Room> loadedRooms = new List<Room>();
+
+    bool isLoadingRoom = false;
+  
+    void Awake()
     {
-        
+        instance = this;
+
+    }
+    public bool DoesRoomExist(int x, int y)
+    {
+        return loadedRooms.Find(item => item.X == x && item.Y == y) != null;
     }
 }
