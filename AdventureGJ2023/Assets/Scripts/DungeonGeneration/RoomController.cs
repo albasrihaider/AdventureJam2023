e9d83578a4506ec6410f039e3b45b7f8264e027a
Haider Albasri <haiderf3@gmail.com>
2023-06-09 08:08:59 +0300

    Fix room duplication bug

--- AdventureGJ2023/Assets/Scripts/DungeonGeneration/RoomController.cs
+++ AdventureGJ2023/Assets/Scripts/DungeonGeneration/RoomController.cs
@@ -91,24 +91,34 @@ public class RoomController : MonoBehaviour
 
     public void RegisterRoom(Room room)
     {
-        room.transform.position = new Vector3(
-        currentLoadRoomData.X * room.Width,
-        currentLoadRoomData.Y * room.Height, 0
-        );
-
-        room.X = currentLoadRoomData.X;
-        room.Y = currentLoadRoomData.Y;
-        room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.X + ", " + room.Y;
-        room.transform.parent = transform;
-
-        isLoadingRoom = false;
-
-        if (loadedRooms.Count == 0)
+        if (!DoesRoomExist(currentLoadRoomData.X, currentLoadRoomData.Y))
         {
-            CameraController.instance.currRoom = room;
-        }
 
-        loadedRooms.Add(room);
+
+            room.transform.position = new Vector3(
+            currentLoadRoomData.X * room.Width,
+            currentLoadRoomData.Y * room.Height, 0
+            );
+
+            room.X = currentLoadRoomData.X;
+            room.Y = currentLoadRoomData.Y;
+            room.name = currentWorldName + "-" + currentLoadRoomData.name + " " + room.X + ", " + room.Y;
+            room.transform.parent = transform;
+
+            isLoadingRoom = false;
+
+            if (loadedRooms.Count == 0)
+            {
+                CameraController.instance.currRoom = room;
+            }
+
+            loadedRooms.Add(room);
+        }
+        else 
+        {
+            Destroy(room.gameObject);
+            isLoadingRoom = false;
+        }
     }
     public bool DoesRoomExist(int x, int y)
     {
